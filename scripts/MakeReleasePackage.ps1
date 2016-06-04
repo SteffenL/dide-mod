49b0b1303c0d5d3ba3dfb822bf60ff8dce7bf9be
Steffen André Langnes <steffenl.sw@gmail.com>
2016-06-04 02:41:21 +0200

    Exclude test exe when packing for release.

--- scripts/MakeReleasePackage.ps1
+++ scripts/MakeReleasePackage.ps1
@@ -21,7 +21,7 @@ Remove-Item -Path "${tempWorkDir}/*"
 
 
 $packageOutputPath = Join-Path -Path $tempOutputDir -ChildPath "${packageName}-${packageVersion}-bin.zip"
-Copy-Item -Path "bin/Release/*" -Include ("*.dll", "*.exe") -Destination $tempWorkDir
+Copy-Item -Path "bin/Release/*" -Include ("*.dll", "*.exe") -Exclude ("*_test.*") -Destination $tempWorkDir
 Copy-Item -Path "bundled/*" -Destination $tempWorkDir
 [System.IO.Compression.ZipFile]::CreateFromDirectory($tempWorkDir, $packageOutputPath)
 Remove-Item -Path "${tempWorkDir}/*"
