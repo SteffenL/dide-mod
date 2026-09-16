91df0fe375d141424d645611ee377ba5179ad9d7
Steffen André Langnes <steffenl.sw@gmail.com>
2026-09-16 23:05:37 +0900

    Remove VS project and vendored libraries

--- core/GenerateVersion.ps1
+++
@@ -1,5 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$scmCommit = &git rev-parse HEAD
-$content = "#pragma once`n#define CORE_VERSION_SCM_COMMIT ""${scmCommit}""`n"
-$content | Set-Content -Path "src/CoreVersion_generated.h"
--- core/src/CoreVersion.cpp
+++
@@ -1,7 +0,0 @@
-#include <core/CoreVersion.h>
-#include "CoreVersion_generated.h"
-
-extern std::string CoreGetVersion()
-{
-	return CORE_VERSION_SCM_COMMIT;
-}
